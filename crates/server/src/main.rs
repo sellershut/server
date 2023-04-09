bcd7b3b1fde79fdb579870a94e48dd13b53b5ea5
kawaki-san <dev@kanjala.com>
2023-04-09 12:26:38 +0200

    start webserver

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+
+use axum::{response::Html, routing, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", routing::get(handler));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Server is live on {address}");
+
+    if let Err(e) = axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+    {
+        eprintln!("{e}");
+    }
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<p>Hello server</p>")
 }
