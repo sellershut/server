80b50484187893f917b6e115a8590101483034dc
kawaki-san <dev@kanjala.com>
2023-04-11 23:49:03 +0200

    [mutation]: update user

--- crates/api/src/mutation/user/mod.rs
+++ crates/api/src/mutation/user/mod.rs
@@ -2,7 +2,7 @@ mod input;
 
 use api_core::Mutation;
 use async_graphql::{Context, Object, Result};
-use entity::{async_graphql, user};
+use entity::{async_graphql, sea_orm::prelude::Uuid, user};
 
 use crate::Database;
 
@@ -17,5 +17,17 @@ impl UserMutation {
         let conn = Database::get_connection_from_context(ctx)?;
 
         Ok(Mutation::create_user(conn, input.into_model_with_arbitrary_id()).await?)
+    }
+
+    async fn update_user(
+        &self,
+        ctx: &Context<'_>,
+        id: String,
+        input: CreateUserInput,
+    ) -> Result<user::Model> {
+        let conn = Database::get_connection_from_context(ctx)?;
+        let id = Uuid::parse_str(&id)?;
+
+        Ok(Mutation::update_user(conn, id, input.into_model_with_arbitrary_id()).await?)
     }
 }
--- crates/core/src/mutation/user/mod.rs
+++ crates/core/src/mutation/user/mod.rs
@@ -1,5 +1,6 @@
+mod input;
 use entity::{
-    sea_orm::{prelude::Uuid, DbConn, DbErr, EntityTrait, Set},
+    sea_orm::{prelude::Uuid, ActiveModelTrait, DbConn, DbErr, EntityTrait, Set},
     user,
     user::Entity as User,
 };
@@ -23,5 +24,38 @@ impl Mutation {
             id: res.last_insert_id,
             ..form_data
         })
+    }
+
+    pub async fn update_user(
+        db: &DbConn,
+        id: Uuid,
+        form_data: user::Model,
+    ) -> Result<user::Model, DbErr> {
+        if let Some(user) = User::find_by_id(id).one(db).await? {
+            // Into ActiveModel
+            let mut user: user::ActiveModel = user.into();
+            if let Some(ref name) = form_data.name {
+                user.name = Set(Some(name.to_owned()))
+            }
+            if let Some(ref email_verified) = form_data.email_verified {
+                user.email_verified = Set(Some(email_verified.to_owned()))
+            }
+            if let Some(ref email) = form_data.email {
+                user.email = Set(Some(email.to_owned()))
+            }
+            if let Some(ref saved_ads) = form_data.saved_ads {
+                user.saved_ads = Set(Some(saved_ads.to_owned()))
+            }
+            if let Some(ref image) = form_data.image {
+                user.image = Set(Some(image.to_owned()))
+            }
+            if let Some(ref watched_categories) = form_data.watched_categories {
+                user.watched_categories = Set(Some(watched_categories.to_owned()))
+            }
+
+            Ok(user.update(db).await?)
+        } else {
+            Err(DbErr::RecordNotFound(id.to_string()))
+        }
     }
 }
