1ab39a01bcf714cf4fe08b04f63402f24f309cf6
kawaki-san <dev@kanjala.com>
2023-04-12 01:26:16 +0200

    [query]: find user by account

--- crates/api/src/query/user.rs
+++ crates/api/src/query/user.rs
@@ -33,4 +33,15 @@ impl UserQuery {
 
         Ok(Query::find_user_by_email(conn, email).await?)
     }
+
+    async fn get_user_by_account(
+        &self,
+        ctx: &Context<'_>,
+        provider: String,
+        provider_account_id: String,
+    ) -> Result<Option<user::Model>, DbErr> {
+        let conn = Database::get_connection_from_context(ctx)?;
+
+        Ok(Query::find_user_by_account(conn, provider, provider_account_id).await?)
+    }
 }
--- crates/core/src/query/user.rs
+++ crates/core/src/query/user.rs
@@ -1,5 +1,6 @@
 use entity::{
-    sea_orm::{prelude::Uuid, DbConn, DbErr, EntityTrait},
+    account::Entity as Account,
+    sea_orm::{prelude::Uuid, DbConn, DbErr, EntityTrait, ModelTrait},
     user::{self, Entity as User},
 };
 
@@ -17,5 +18,25 @@ impl Query {
         email: String,
     ) -> Result<Option<user::Model>, DbErr> {
         User::find_by_email(email).one(db).await
+    }
+
+    pub async fn find_user_by_account(
+        db: &DbConn,
+        provider: String,
+        provider_account_id: String,
+    ) -> Result<Option<user::Model>, DbErr> {
+        // Find a cake model first
+        if let Some(account) =
+            Account::find_by_provider(provider.clone(), provider_account_id.clone())
+                .one(db)
+                .await?
+        {
+            // Then, find all related fruits of this cake
+            Ok(account.find_related(User).one(db).await?)
+        } else {
+            Err(DbErr::RecordNotFound(format!(
+                "no user found with account provider: {provider} with id {provider_account_id}"
+            )))
+        }
     }
 }
